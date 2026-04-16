fa31e67f32a3ff80d874cce9d9c6218f6fd45f2f
swati-zampal <131242062+swati-zampal@users.noreply.github.com>
2026-04-16 16:08:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Respond with "Hello World" when a GET request is made to the root "/"
 app.get('/', (req, res) => {
-  res.send('Welcome To Nodejs App');
+  res.send('Welcome To My Nodejs App');
 });
 
 // Start server on port 3000 or custom port via environment variable
